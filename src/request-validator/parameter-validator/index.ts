64c0b90b221d9f890b2ef8e16a86296da240ff28
Freddy Fallon <freddyfallon@gmail.com>
2018-10-12 11:19:39 +0100

    ✨ Allow parameterValidator to handle $refs

--- src/request-validator/parameter-validator/index.ts
+++ src/request-validator/parameter-validator/index.ts
@@ -1,14 +1,18 @@
 import { filter, propEq, compose, find } from 'ramda'
-import { Parameter } from '../../interfaces'
+import { Parameter, OpenAPI } from '../../interfaces'
 import checkHeaders from './check-headers'
-export default (req: any, parameters: [Parameter]): any => {
+export default (
+  req: any,
+  parameters: [Parameter],
+  swaggerDoc: OpenAPI
+): any => {
   if (parameters) {
     const headerParams = find(propEq('in', 'header'))(parameters)
       ? filter(propEq('in', 'header'))(parameters)
       : undefined
 
     const checkParams = compose(checkHeaders)
-    return checkParams(headerParams, req.headers)
+    return checkParams(headerParams, req.headers, swaggerDoc)
   }
   return true
 }
