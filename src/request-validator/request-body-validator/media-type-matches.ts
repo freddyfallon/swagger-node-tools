830391f2751207c12189fa23a7e2e04134d3a140
Jake Rowsell <Jake.Rowsell@bglgroup.co.uk>
2018-10-12 13:34:15 +0100

    Fix body required functionality & additional scenario testing

--- src/request-validator/request-body-validator/media-type-matches.ts
+++ src/request-validator/request-body-validator/media-type-matches.ts
@@ -1,12 +1,11 @@
 import { OpenAPI, RequestBody } from '../../interfaces'
 
-export default (req: any, requestBody: RequestBody): boolean => {
+export default (contentType: any = '', requestBody: RequestBody): boolean => {
   let contentMap = requestBody.content
-  const reqMediaType = req.headers['content-type']
 
   if (!(requestBody.content instanceof Map)) {
     contentMap = new Map(Object.entries(requestBody.content || {}))
   }
 
-  return contentMap.has(reqMediaType)
+  return contentMap.has(contentType)
 }
