cf40727a9e286b7ae69f56a9114ea4a819f54b92
Freddy Fallon <freddyfallon@gmail.com>
2018-10-12 09:12:46 +0100

    ✨ Add handling of headeer types

--- src/request-validator/parameter-validator.ts
+++ src/request-validator/parameter-validator.ts
@@ -1,2 +1,22 @@
+import { filter, propEq, type, compose, find } from 'ramda'
 import { Parameter } from '../interfaces'
-export default (req: any, parameters: [Parameter]): boolean => true
+export default (req: any, parameters: [Parameter]): any => {
+  if (parameters) {
+    const headerParams = find(propEq('in', 'header'))(parameters)
+      ? filter(propEq('in', 'header'))(parameters)
+      : null
+    const checkHeaders = () =>
+      headerParams
+        ? headerParams.reduce((validated: boolean, currentHeader: any) => {
+            if (
+              currentHeader.schema.type !== type(req.headers[currentHeader])
+            ) {
+              return false
+            }
+          }, true)
+        : true
+    const result = compose(checkHeaders)
+    return result()
+  }
+  return true
+}
