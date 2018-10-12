c34308455c51b0d102eef1b0e88a2a8ba5d0b2e6
Freddy Fallon <freddyfallon@gmail.com>
2018-10-12 11:19:39 +0100

    ✨ Allow parameterValidator to handle $refs

--- src/request-validator/parameter-validator/check-headers.ts
+++ src/request-validator/parameter-validator/check-headers.ts
@@ -1,19 +1,46 @@
 import { type } from 'ramda'
-export default (headerParameters: any, requestHeaders: any) =>
+import getRef from '../../helpers/get-ref'
+import { OpenAPI } from '../../interfaces'
+
+export default (
+  headerParameters: any,
+  requestHeaders: any,
+  swaggerDoc: OpenAPI
+) =>
   headerParameters
     ? headerParameters.reduce((result: any, currentHeader: any) => {
-        if (
+        if (currentHeader.schema.$ref) {
+          const schema: any = getRef(swaggerDoc, currentHeader.schema.$ref)
+          if (
+            schema.type !==
+            type(requestHeaders[currentHeader.name]).toLowerCase()
+          ) {
+            throwErrorForWrongType(
+              type(requestHeaders[currentHeader.name]),
+              currentHeader.name,
+              schema.type
+            )
+          }
+        } else if (
           currentHeader.schema.type !==
           type(requestHeaders[currentHeader.name]).toLowerCase()
         ) {
-          throw new Error(
-            `Found type of ${type(
-              requestHeaders[currentHeader.name]
-            ).toLowerCase()} for ${currentHeader.name}, expected ${
-              currentHeader.schema.type
-            }`
+          throwErrorForWrongType(
+            type(requestHeaders[currentHeader.name]),
+            currentHeader.name,
+            currentHeader.schema.type
           )
         }
         return result
       }, true)
     : true
+
+const throwErrorForWrongType = (
+  typeFound: string,
+  propertyName: string,
+  expectedType: string
+) => {
+  throw new Error(
+    `Found type of ${typeFound.toLowerCase()} for ${propertyName}, expected ${expectedType}`
+  )
+}
--- src/request-validator/parameter-validator/index.test.ts
+++ src/request-validator/parameter-validator/index.test.ts
@@ -1,4 +1,5 @@
 import parameterValidator from './'
+import minimalSwaggerDoc from '../../minimal-swagger'
 
 describe('parameterValidator', () => {
   test('returns true when no parameters are provided', () => {
@@ -8,18 +9,19 @@ describe('parameterValidator', () => {
       }
     }
     const requiredParams: any = undefined
-    const result = parameterValidator(req, requiredParams)
+    const result = parameterValidator(req, requiredParams, minimalSwaggerDoc)
     expect(result).toBe(true)
   })
 
   test('returns true when no headers or parameters are provided', () => {
     const req = {}
     const requiredParams: any = undefined
-    const result = parameterValidator(req, requiredParams)
+    const result = parameterValidator(req, requiredParams, minimalSwaggerDoc)
     expect(result).toBe(true)
   })
+
   describe('when parameter is in header and is required', () => {
-    test('will return false if parameter is present as integer in request where string is required', () => {
+    test('will throw error if parameter is present as integer in request where string is required', () => {
       const req = {
         headers: {
           cool_header: 1234
@@ -34,8 +36,9 @@ describe('parameterValidator', () => {
           }
         }
       ]
+
       try {
-        parameterValidator(req, requiredParams)
+        parameterValidator(req, requiredParams, minimalSwaggerDoc)
       } catch (err) {
         expect(err).toEqual(
           Object.assign(
@@ -44,7 +47,7 @@ describe('parameterValidator', () => {
         )
       }
     })
-    test('will return false if parameter is present as boolean in request where string is required', () => {
+    test('will throw error if parameter is present as boolean in request where string is required', () => {
       const req = {
         headers: {
           cool_header: false
@@ -59,8 +62,9 @@ describe('parameterValidator', () => {
           }
         }
       ]
+
       try {
-        parameterValidator(req, requiredParams)
+        parameterValidator(req, requiredParams, minimalSwaggerDoc)
       } catch (err) {
         expect(err).toEqual(
           Object.assign(
@@ -69,7 +73,7 @@ describe('parameterValidator', () => {
         )
       }
     })
-    test('will return true if parameter is present as correct type', () => {
+    test('will throw error if parameter is present as correct type', () => {
       const send = jest.fn()
       const status = jest.fn().mockReturnValue(send)
       const req = {
@@ -86,9 +90,107 @@ describe('parameterValidator', () => {
           }
         }
       ]
-      const result = parameterValidator(req, requiredParams)
+
+      const result = parameterValidator(req, requiredParams, minimalSwaggerDoc)
       expect(result).toBe(true)
     })
+
+    test('will throw error if parameter is present as correct type in a $ref', () => {
+      const req = {
+        headers: {
+          cool_header: 'hello'
+        }
+      }
+      const requiredParams: any = [
+        {
+          name: 'cool_header',
+          in: 'header',
+          schema: { $ref: '#/components/schemas/coolHeaderSchema' }
+        }
+      ]
+
+      const paths = { '/': {} }
+      const schemas = new Map()
+      schemas.set('coolHeaderSchema', {
+        type: 'string'
+      })
+
+      const swaggerDoc = {
+        ...minimalSwaggerDoc,
+        paths,
+        components: {
+          schemas
+        }
+      }
+
+      const result = parameterValidator(req, requiredParams, swaggerDoc)
+      expect(result).toBe(true)
+    })
+
+    test('will throw error if parameter is present as incorrect type in a $ref', () => {
+      const req = {
+        headers: {
+          cool_header: 123
+        }
+      }
+      const requiredParams: any = [
+        {
+          name: 'cool_header',
+          in: 'header',
+          schema: { $ref: '#/components/schemas/coolHeaderSchema' }
+        }
+      ]
+
+      const paths = { '/': {} }
+      const schemas = new Map()
+      schemas.set('coolHeaderSchema', {
+        type: 'string'
+      })
+
+      const swaggerDoc = {
+        ...minimalSwaggerDoc,
+        paths,
+        components: {
+          schemas
+        }
+      }
+
+      try {
+        parameterValidator(req, requiredParams, swaggerDoc)
+      } catch (err) {
+        expect(err).toEqual(
+          Object.assign(
+            Error('Found type of number for cool_header, expected string')
+          )
+        )
+      }
+    })
+
+    test('will throw error if parameter is marked as required but are not given', () => {
+      const req = {
+        headers: {}
+      }
+      const requiredParams: any = [
+        {
+          name: 'cool_header',
+          in: 'header',
+          require: true,
+          schema: {
+            type: 'string'
+          }
+        }
+      ]
+      try {
+        parameterValidator(req, requiredParams, minimalSwaggerDoc)
+      } catch (err) {
+        expect(err).toEqual(
+          Object.assign(
+            Error('Found type of number for cool_header, expected string')
+          )
+        )
+      }
+    })
+
     describe('when parameter is in query', () => {})
     describe('when parameter is in path', () => {})
     describe('when parameter is in cookie', () => {})
